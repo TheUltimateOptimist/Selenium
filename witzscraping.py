453f31e8ffb8b5136fe9e78d629acf87d122fb16
TheUltimateOptimist <digitalConfidence@web.de>
2021-09-08 11:43:32 +0200

    scraped two jokes categorys

--- witzscraping.py
+++ witzscraping.py
@@ -3,24 +3,47 @@ import fundamentals as f
 indexList = []
 
 
-def writeWitz(index):
+def writeWitz(index, file):
     print(index)
-    witz = f.findElement(
-        f"/html/body/div[1]/div[3]/div[1]/div/blockquote[{str(index)}]/p", "x-path").text
-    print(witz)
-    if witz != "":
-        file.write(witz + "\n")
+    if index == 2:
         return True
     else:
-        return False
+        witz = f.findElement(
+            f"/html/body/div[1]/div[3]/div[1]/div/blockquote[{str(index)}]/p", "x-path").text
+        print(witz)
+        if witz != "":
+            file.write(witz + "\n")
+            return True
+        else:
+            return False
 
 
-f.openAnyWebsite("https://www.witzepause.com/akademiker-witze")
-file = open("akademiker.txt", "a", encoding="utf-8")
-for i in range(1, 41):
-    if writeWitz(i) == False:
-        indexList.append(i)
+def removeDoubleLines():
+    realFile = open("allekinder.txt", "r")
+    lines = realFile.readlines()
+    witze = []
+    print("Lines: " + str(lines))
+    for line in lines:
+        if witze.__contains__(line) == False:
+            witze.append(line)
+    file = open("allekinder-kategorie.txt", "a")
+    print("witze: " + str(witze))
+    for witz in witze:
+        file.write(witz)
+
+
+f.openAnyWebsite("https://www.witzepause.com/alle-kinder-sprueche")
+file = open("allekinder.txt", "a", encoding="utf-8")
+try:
+    for i in range(1, 200):
+        if writeWitz(i, file) == False:
+            indexList.append(i)
+except:
+    print("an error ocurred")
+print(indexList)
 while len(indexList) > 0:
     for index in indexList:
-        if writeWitz(i) == True:
-            indexList.remove(i)
+        if writeWitz(index, file) == True:
+            indexList.remove(index)
+            print(indexList)
+removeDoubleLines()
