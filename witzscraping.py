3ed978e05f99939eca651b773a240fbd53f2300a
TheUltimateOptimist <digitalConfidence@web.de>
2021-09-09 11:32:13 +0200

    scraped all jokes

--- witzscraping.py
+++ witzscraping.py
@@ -10,7 +10,7 @@ def writeWitz(index, file):
     else:
         witz = f.findElement(
             f"/html/body/div[1]/div[3]/div[1]/div/blockquote[{str(index)}]/p", "x-path").text
-        print(witz)
+        witz = " ".join(witz.split("\n"))
         if witz != "":
             file.write(witz + "\n")
             return True
@@ -19,14 +19,14 @@ def writeWitz(index, file):
 
 
 def removeDoubleLines(newFilename, oldFilename):
-    realFile = open(f"{oldFilename}.txt", "r")
+    realFile = open(f"{oldFilename}.txt", "r", encoding="utf-8")
     lines = realFile.readlines()
     witze = []
     print("Lines: " + str(lines))
     for line in lines:
         if witze.__contains__(line) == False:
             witze.append(line)
-    file = open(f"{newFilename}.txt", "a")
+    file = open(f"{newFilename}.txt", "a", encoding="utf-8")
     print("witze: " + str(witze))
     for witz in witze:
         file.write(witz)
@@ -52,16 +52,43 @@ def scrapeJokesOfSite(url, filename):
 
 def scrapeJokesOfAllSites(url, filename):
     try:
+        fileLength = 0
         for i in range(1, 200):
             if i > 1:
                 addition = f"?page={i}"
             else:
                 addition = ""
             scrapeJokesOfSite(url + addition, filename)
+            file = open(f"{filename}.txt", "r", encoding="utf-8")
+            lines = file.readlines()
+            if len(lines) > fileLength:
+                fileLength = len(lines)
+            else:
+                break
     except:
         print("all pages scraped")
 
 
-scrapeJokesOfAllSites(
-    "https://www.witzepause.com/arbeitswitze", "arbeitswitze")
-removeDoubleLines("arbeitswitze-kategorie", "arbeitswitze")
+def removeLineValues(filename, lineValue):
+    file = open(filename, "r", encoding="utf-8")
+    lines = file.readlines()
+    file.close()
+    lines.remove(lineValue)
+    newFile = open("new-" + filename, "a", encoding="utf-8")
+    for line in lines:
+        newFile.write(line)
+    import os
+    os.remove(filename)
+    os.rename("new-" + filename, filename)
+
+
+listOfCategories = ["akademiker-witze", "alle-kinder-sprueche", "arztwitze", "anwaltswitze", "arbeitswitze", "arztwitze", "bankerwitze", "bauernwitze", "bayern-witze", "beamtenwitze", "blondinenwitze", "boese-witze", "bundeswehr-witze", "buerowitze", "chinesen-witze", "computerwitze", "deine-mutter-witze", "deutsche-witze", "drogen-witze", "familienwitze", "flachwitze", "fussballwitze", "geschichte-witze",
+                    "jaegerwitze", "juristen-witze", "kannibalen-witze", "kellnerwitze", "kinderwitze", "laenderwitze", "musikerwitze", "oesterreicher-witze", "ostfriesenwitze", "polenwitze", "politiker-witze", "polizei-witze", "schlechte-witze", "schulwitze", "sportwitze", "studentenwitze", "tierwitze", "urlaubswitze", "weihnachtswitze", "wortwitze"]
+# print(len(listOfCategories))
+# for category in listOfCategories:
+#     scrapeJokesOfAllSites(f"https://www.witzepause.com/{category}", category)
+#     removeDoubleLines(f"{category}-kategorie", f"{category}")
+
+for i, category in enumerate(listOfCategories):
+    if i > 10:
+        print("")
